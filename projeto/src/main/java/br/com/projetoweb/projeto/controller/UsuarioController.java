898365c666f21d84b08b8584d137e5544b271db6
Matheus Felipe <matheusbarros9107@gmail.com>
2023-03-25 16:05:43 -0300

    update cadastro
    funcionalidade cadastro está funcionando.

--- projeto/src/main/java/br/com/projetoweb/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/projetoweb/projeto/controller/UsuarioController.java
@@ -1,21 +1,55 @@
 package br.com.projetoweb.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.projetoweb.projeto.DAO.UsuarioRepository;
 import br.com.projetoweb.projeto.model.Usuario;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/usuarios")
 public class UsuarioController{
 	@Autowired
 	private UsuarioRepository dao;
 	
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> listaUsuarios(){
 		return (List<Usuario>) dao.findAll();		
+	}
+	
+	@PostMapping
+	public Usuario criarUsuario(@RequestBody Usuario usuario) {
+		Usuario usuarioNovo = dao.save(usuario);
+		return usuarioNovo;
+	}
+	
+	@PutMapping 
+	public Usuario EditarUsuario (@RequestBody Usuario usuario) {
+		Usuario usuarioEditado = dao.save(usuario);
+		return usuarioEditado;
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> excluirUsuario(@PathVariable Integer id) {
+	    Optional<Usuario> usuarioExcluido = dao.findById(id);
+	    if (usuarioExcluido.isPresent()) {
+	        dao.deleteById(id);
+	        return ResponseEntity.ok("Usuário excluído com sucesso.");
+	    } else {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+	    }
 	}
 }
--- script.js
+++ script.js
@@ -1,41 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('button');
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const senha = document.getElementById('senha')
+    const telefone = document.getElementById('telefone')
 
     button.addEventListener('click', function(event) {
         event.preventDefault(); 
         const form = document.getElementById('form');
         if (form.checkValidity()) {
-            const nome = document.getElementById('nome').value;
-            const email = document.getElementById('email').value;
-            const senha = document.getElementById('senha').value;
-            const telefone = document.getElementById('telefone').value;
-
-            const dados = {
-                nome:nome,
-                email:email,
-                senha:senha,
-                telefone:telefone
-            };
             cadastrar();
             limpar();
-            alert("Cadastro Realizado com Sucesso!")
         } else {
             alert("Por favor, preencha todos os campos corretamente.");
         }
     });
 
     function cadastrar(){
-        fetch("http://localhost:8050/cadastrar", {
+        fetch("http://localhost:8050/usuarios", 
+        {
             headers: {
                 'Accept': 'application/json',
                 'content-Type': 'application/json'
             },
             method: "POST",
             body: JSON.stringify({
-                nome: nome,
-                email: email,
-                senha: senha,
-                telefone: telefone
+                nome: nome.value,
+                email: email.value,
+                senha: senha.value,
+                telefone: telefone.value
             })
         })
         .then(function(res) {
